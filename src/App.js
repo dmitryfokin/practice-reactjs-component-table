cb51ba287a849dea700626df7910d6d4d3c8a4c5
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-06-10 14:57:48 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Практика. Создание компонента универсальной таблицы на React JS</h1>
+    </div>
+  )
+}
+
+export default App
